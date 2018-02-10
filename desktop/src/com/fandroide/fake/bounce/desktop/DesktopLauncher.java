19702133225caef29f3e080f6d0d872eb6a7c591
Feliciano Martinez <codice.f.m.s@gmail.com>
2018-02-10 16:31:23 -0600

    Comenzando Proyecto

---
+++ desktop/src/com/fandroide/fake/bounce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fandroide.fake.bounce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fandroide.fake.bounce.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
